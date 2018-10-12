8fb0d9e6c4d7db2403be9399c123a835d149d9e4
Legacy <fabio.cumbo@std.heh.be>
2018-10-12 22:16:04 +0200

    My first app

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+
+export default class App extends React.Component
+{
+  render() {
+    return (
+      <Text>My first app</Text>
+    );
+  }
+}
