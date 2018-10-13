f68a1b97718601156ddf91282a3fb66bf6c18431
Legacy <fabio.cumbo@std.heh.be>
2018-10-13 17:45:42 +0200

    Added helpers for product display test

---
+++ Helpers/data.js
@@ -0,0 +1,11 @@
+export default data = [
+    {
+       imageFood:"https://static.openfoodfacts.org/images/products/302/933/000/3533/front_fr.96.400.jpg",
+       title:"Biscuit",
+       code:"123456789",
+       ingredients:["farine","blé"],
+       imageBack:"https://static.openfoodfacts.org/images/products/302/933/000/3533/nutrition_fr.101.400.jpg"
+    }
+ ]
+ 
+ 
