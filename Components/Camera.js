f555230fd2f1b7cd7c2f1cd72c58fef55602f98e
Fabio Cumbo <legacy1810@gmail.com>
2018-11-19 18:22:32 +0100

    Création de la Vue caméra pour ouvrir la caméra du téléphone et faire le scanning

---
+++ Components/Camera.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { BarCodeScanner } from 'expo';
+
+const { width } = Dimensions.get('window');
+
+export default class Camera extends React.Component {
+
+  render() {
+    return (
+      <BarCodeScanner
+        onBarCodeRead={(scan) => this.props.navigation.navigate("Search", {textScan: scan.data})}
+        style={[StyleSheet.absoluteFill, styles.container]}
+      >
+        <View style={styles.layerTop} />
+        <View style={styles.layerCenter}>
+          <View style={styles.layerLeft} />
+          <View style={styles.focused} />
+          <View style={styles.layerRight} />
+        </View>
+        <View style={styles.layerBottom} />
+      </BarCodeScanner>
+    );
+  }
+}
+
+const opacity = 'rgba(0, 0, 0, .6)';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  layerTop: {
+    flex: 2,
+    backgroundColor: opacity
+  },
+  layerCenter: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  layerLeft: {
+    flex: 1,
+    backgroundColor: opacity
+  },
+  focused: {
+    flex: 10
+  },
+  layerRight: {
+    flex: 1,
+    backgroundColor: opacity
+  },
+  layerBottom: {
+    flex: 2,
+    backgroundColor: opacity
+  },
+});
