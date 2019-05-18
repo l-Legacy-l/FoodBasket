18fd6bf562b40cfb0d55c4999f209f0e1eadbf6d
Fabio Cumbo <legacy1810@gmail.com>
2019-05-18 17:28:09 +0200

    Created Tab navigation for Search and MyBasket components

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,36 +1,50 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createAppContainer, createBottomTabNavigator } from 'react-navigation';
 import Search from '../Components/Search';
 import Camera from '../Components/Camera';
 import AddFoodItem from '../Components/AddFoodItem';
+import MyBasket from '../Components/MyBasket';
 
-const SearchStackNavigator = createStackNavigator(
-  {
-    // Correspond à la vue du component Search (on met le nom qu'on veut)
-    Search:
-    {
-      screen: Search,
-      navigationOptions:
-        {
-          title: 'Search',
-        },
-    },
-
-    Camera:
-    {
-      screen: Camera,
-    },
-
-    AddFoodItem:
-    {
-      screen: AddFoodItem,
-      navigationOptions:
-        {
-          title: 'Add an item',
-        },
+const SearchStackNavigator = createStackNavigator({
+  // Match the component Search (name is free)
+  Search: {
+    screen: Search,
+    navigationOptions: {
+      title: 'Rechercher un produit',
     },
   },
-);
 
-const App = createAppContainer(SearchStackNavigator);
+  Camera: {
+    screen: Camera,
+  },
+
+  AddFoodItem: {
+    screen: AddFoodItem,
+    navigationOptions: {
+      title: 'Ajouter un produit',
+    },
+  },
+});
+
+const MyBasketStackNavigator = createStackNavigator({
+  MyBasket: {
+    screen: MyBasket,
+    navigationOptions: {
+      title: 'Mes aliments',
+    },
+  },
+});
+
+const MainTabNavigator = createBottomTabNavigator({
+  Search: {
+    screen: SearchStackNavigator,
+  },
+
+  MyBasket: {
+    screen: MyBasketStackNavigator,
+  },
+});
+
+
+const App = createAppContainer(MainTabNavigator);
 
 export default App;
