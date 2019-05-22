928f850a646f14e88d0a6ee70f08e6c3d13b714a
Fabio Cumbo <legacy1810@gmail.com>
2019-05-22 15:54:06 +0200

    Added buttons to edit each food quantity in the list

--- Components/MyBasket.js
+++ Components/MyBasket.js
@@ -1,17 +1,68 @@
-import React, { PureComponent } from 'react';
-import { ScrollView } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import React, { Component } from 'react';
+import { ScrollView, View, Alert } from 'react-native';
+import { ListItem, Icon } from 'react-native-elements';
+import DialogInput from 'react-native-dialog-input';
+import Toast from 'react-native-simple-toast';
+import _ from 'lodash';
 
-export default class MyBasket extends PureComponent {
-  render() {
-    return (
-      <ScrollView>
-        {
-          this.props.screenProps.foodList.map(item => (
+
+export default class MyBasket extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRemoveDialogVisible: false,
+      isAddDialogVisible: false,
+    };
+    this.foodListItem = {};
+  }
+
+    sendInput = (inputText, operation) => {
+    // eslint-disable-next-line radix
+      const inputNumber = parseInt(inputText);
+
+      // Check if inputText is a valid number
+      if (!Number.isNaN(inputNumber) && inputNumber >= 1 && inputNumber <= 20) {
+        const { screenProps } = this.props;
+        let foodName = '';
+        foodName = this.foodListItem.name !== undefined ? this.foodListItem.name : '';
+        const foodListTemp = screenProps.foodList;
+        const foodListItemIndex = _.findIndex(screenProps.foodList, foodListItem => foodListItem.barcode === this.foodListItem.barcode);
+
+        let quantity = parseInt(foodListTemp[foodListItemIndex].quantity, 10);
+
+        if (operation === 'add') {
+          quantity += inputNumber;
+          foodListTemp[foodListItemIndex].quantity = quantity;
+        } else {
+          quantity -= inputNumber;
+          if (quantity <= 0) {
+            // delete the food object of the foodList array
+            foodListTemp.splice(foodListItemIndex, 1);
+          } else {
+            foodListTemp[foodListItemIndex].quantity = quantity;
+          }
+        }
+
+        screenProps.updateFoodList(foodListTemp);
+        this.setState({ isAddDialogVisible: false, isRemoveDialogVisible: false });
+        Toast.show(`La quantité de ${foodName} a bien été modifée`);
+      } else {
+        this.setState({ isAddDialogVisible: false, isRemoveDialogVisible: false });
+        Toast.show('Vous devez rentrer un entier valide compris entre 1 et 20');
+      }
+    }
+
+    render() {
+      const { screenProps } = this.props;
+      return (
+        <View>
+          <ScrollView>
+            {
+          screenProps.foodList.map(item => (
             <ListItem
               key={item.barcode}
               title={item.name}
-              titleStyle={{ fontSize: 22 }}
+              titleStyle={{ fontSize: 20 }}
               leftAvatar={{
                 source: { uri: item.image },
                 size: 'large',
@@ -22,14 +73,97 @@ export default class MyBasket extends PureComponent {
               }}
               subtitle={item.barcode}
               subtitleStyle={{ marginTop: 10 }}
-              badge={{ value: item.quantity, badgeStyle: { backgroundColor: '#517fa4', width: 40, height: 25 }, textStyle: { fontSize: 18 } }}
               chevron
               bottomDivider
+              rightIcon={(
+                <View>
+                  <Icon
+                    reverse
+                    name="delete"
+                    type="material-community"
+                    color="#517fa4"
+                    size={16}
+                    onPress={() => {
+                      Alert.alert(
+                        'Confirmation de suppression',
+                        'Voulez-vous supprimer ce produit de votre liste ?',
+                        [
+                          {
+                            text: 'Non',
+                            style: 'cancel',
+                          },
+                          {
+                            text: 'Oui',
+                            onPress: () => {
+                              let foodName = '';
+                              foodName = item.product_name_fr !== undefined ? item.product_name_fr : '';
+                              const foodListTemp = screenProps.foodList;
+                              const foodListItemIndex = _.findIndex(screenProps.foodList, foodListItem => foodListItem.barcode === item.barcode);
+                              foodListTemp.splice(foodListItemIndex, 1);
+                              screenProps.updateFoodList(foodListTemp);
+                              Toast.show(`Le produit ${foodName} a bien été supprimée`);
+                            },
+                          },
+                        ],
+                        { cancelable: true },
+                      );
+                    }}
+                  />
+                  <Icon
+                    reverse
+                    name="minus"
+                    type="material-community"
+                    color="#517fa4"
+                    size={16}
+                    onPress={() => {
+                      this.foodListItem = item;
+                      this.setState({ isRemoveDialogVisible: true });
+                    }}
+                  />
+                  <Icon
+                    reverse
+                    name="plus"
+                    type="material-community"
+                    color="#517fa4"
+                    size={16}
+                    onPress={() => {
+                      this.foodListItem = item;
+                      this.setState({ isAddDialogVisible: true });
+                    }}
+                  />
+                </View>
+              )}
+              badge={{ value: item.quantity, badgeStyle: { backgroundColor: '#517fa4', width: 40, height: 25 }, textStyle: { fontSize: 18 } }}
+
               onPress={() => console.log('press')}
             />
           ))
         }
-      </ScrollView>
-    );
-  }
+          </ScrollView>
+          {/*            TODO REWORK
+           A bug appear when DialoInput is into the ScrollView component */}
+          <DialogInput
+            isDialogVisible={this.state.isRemoveDialogVisible}
+            title="Quantité à supprimer"
+            message="Entrer la quantité du produit à supprimer de la liste"
+            submitText="Supprimer"
+            cancelText="Annuler"
+            textInputProps={{ keyboardType: 'numeric' }}
+            submitInput={inputText => this.sendInput(inputText, 'delete')}
+            closeDialog={() => this.setState({ isRemoveDialogVisible: false })}
+          />
+
+          <DialogInput
+            isDialogVisible={this.state.isAddDialogVisible}
+            title="Quantité à ajouter"
+            message="Entrer la quantité du produit à ajouter à la liste"
+            submitText="Ajouter"
+            cancelText="Annuler"
+            textInputProps={{ keyboardType: 'numeric' }}
+            submitInput={inputText => this.sendInput(inputText, 'add')}
+            closeDialog={() => this.setState({ isAddDialogVisible: false })}
+          />
+        </View>
+      );
+    }
 }
